3814f595f105c713970a4e408a07fa33e03b89e0
Sangmeshwar Ramesh Patil <89923434+Sangmeshwar96@users.noreply.github.com>
2023-01-18 22:40:41 +0530

    [Sangmeshwar] Add: Adding And Retrieving All Employee Payroll Data From Database

---
+++ EmployeePayrollADO/EmployeePayrollADO/Option.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeePayrollADO
+{
+    public class Option
+    {
+        EmployeePayrollOperations operations = new EmployeePayrollOperations();
+        public int choice;
+        public void CRUDOperation()
+        {
+            do
+            {
+                Console.Write("\n1. Add Employee Payroll Data\n" +
+                    "2. Retrieve All Records\n" +
+                    "0. Exit\n" +
+                    "Select One Option: ");
+                choice = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("________________________________________");
+                switch (choice)
+                {
+                    case 1:
+                        operations.AddEmployeeDetails();
+                        break;
+                    case 2:
+                        operations.RetrieveEmployeeDetails();
+                        if ((operations.empList.Count) > 0)
+                        {
+                            foreach (Employee employee in operations.empList)
+                            {
+                                Console.WriteLine("Employee Id: " + employee.EmpId);
+                                Console.WriteLine("Name: " + employee.Name);
+                                Console.WriteLine("Gender: " + employee.Gender);
+                                Console.WriteLine("Phone: " + employee.Phone);
+                                Console.WriteLine("Address: " + employee.Address);
+                                Console.WriteLine("Department: " + employee.Department);
+                                Console.WriteLine("Salary: " + employee.Salary);
+                                Console.WriteLine("StartDate: " + employee.Startdate);
+                                Console.WriteLine("________________________________________\n");
+                            }
+                        }
+                        else
+                            Console.WriteLine("-----Data Not Found-----");
+                        break;
+                    case 0:
+                        Console.WriteLine("-----Thankyou-----");
+                        break;
+                    default:
+                        Console.WriteLine("-----Invalid Option-----");
+                        break;
+                }
+            }
+            while (choice != 0);
+        }
+    }
+}
